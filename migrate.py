f4e08f9efab4a5b541d2ea862c3d3004e8b411e9
Perceval Arenou <perceval.arenou@gmail.com>
2018-05-26 03:43:04 +0200

    migrate migrate.py to sqlite

--- migrate.py
+++ migrate.py
@@ -4,32 +4,17 @@
 
 import telegram
 from lunchbot import TOKEN
-
-USERS = {}
+import dbconnector
 
 NOTICE = """
-Bonjour %s, J'ai été mis à jour ! 🔄🎇🆙🎆🆕🎈🎊🎉
-Cette mise à jour augmente ma stabilité et mes performances. 
-
-Pour permettre cette progression, le format et le stockage des données ont été améliorés. \
-Cependant, la migration des comptes n'a pas pu être effectuée et j'en suis navré. 😱😢😭
-
-Pour continuer à utiliser Franklin Bot, il est nécessaire de te réinscrire avec la commande suivante : 
-/register BadgeID Nom Prénom
-
-Désolé pour la gêne occasionnée. 🙃🙃🙃
-Bon Appétit 🍽☕️
-
-@paris_lunch_bot
-
-PS :  Si tu trouves des bugs, n'hésites pas à les remonter sur github :
-https://github.com/percevalsa/lunch-bot/issues/
+Bonjour %s ...
 """
-
 print("\n\tSending update notice tu users...\n")
-bot = telegram.Bot(token=TOKEN)
 
-for u in USERS:
+bot = telegram.Bot(token=TOKEN)
+users = dbconnector.get_users()
+
+for u in users[0]:
 	try:
 		chat = bot.getChat(u)
 		print(u, " ", chat.username, " ", chat.first_name, " ", chat.last_name)
