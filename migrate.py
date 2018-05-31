d628669bbfd91ee7b3e2b51aeab3e29f75249cd9
Perceval Arenou <perceval.arenou@gmail.com>
2018-06-01 01:20:09 +0200

    fix migrate script

--- migrate.py
+++ migrate.py
@@ -9,14 +9,14 @@ import dbconnector
 NOTICE = """
 Bonjour %s ...
 """
-print("\n\tSending update notice tu users...\n")
+print("\n\tSending update notice to users...\n")
 
 bot = telegram.Bot(token=TOKEN)
 users = dbconnector.get_users()
 
-for u in users[0]:
+for u in users:
 	try:
-		chat = bot.getChat(u)
+		chat = bot.getChat(u[0])
 		print(u, " ", chat.username, " ", chat.first_name, " ", chat.last_name)
 		bot.send_message(chat_id=chat.id, text=NOTICE % chat.first_name)
 
